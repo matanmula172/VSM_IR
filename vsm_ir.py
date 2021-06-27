a1a4bd8e8e4a006964dc86246d736399d688bf18
matan <matanmula@mail.tau.ac.il>
2021-06-27 18:31:15 +0300

    init project

---
+++ vsm_ir.py
@@ -0,0 +1,16 @@
+import nltk
+import numpy as np
+import json
+import lxml.html
+
+
+def read_f():
+    with open('./cfc-xml_corrected/cf74.xml', 'r') as f:
+        doc = lxml.html.fromstring(f.read())
+        print(doc)
+        rest = doc.xpath('/root/RECORD')
+        for elem in rest:
+            print(elem)
+
+
+read_f()
